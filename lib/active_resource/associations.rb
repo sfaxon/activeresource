f69e86c3837169a8d70db633268a9d71561b9715
Seth Faxon <seth.faxon@gmail.com>
2012-04-16 18:17:32 -0700

    has_one association builds singelton url if attribute not present

--- lib/active_resource/associations.rb
+++ lib/active_resource/associations.rb
@@ -63,6 +63,16 @@ module ActiveResource::Associations
   #
   # <tt>has_one :author, :class_name => 'myblog/author'</tt>
   # Would resolve this author into the <tt>Myblog::Author</tt> class.
+  #
+  # If the response body does not contain an attribute matching the association name
+  # a request is sent to a singelton path under the current resource.
+  # For example, if a Product class <tt>has_one :inventory</tt> calling <tt>Product#inventory</tt>
+  # will generate a request on /product/:product_id/inventory.json.
+  # This requires that the associated class defines a prefix in order for the path to be built.
+  # In this case it would be: <tt>Inventory.prefix = "/product/:product_id/"</tt>.
+  # See the ActiveResource::Singleton::ClassMethods documentation for more information on setting a prefix.
+  # If the associated class does not have a prefix MissingPrefixParam will be raised.
+  #
   def has_one(name, options = {})
     Builder::HasOne.build(self, name, options)
   end
@@ -134,6 +144,23 @@ module ActiveResource::Associations
         attributes[method_name]
       else
         instance_variable_set(ivar_name, association_model.find(:all, :params => {:"#{self.class.element_name}_id" => self.id}))
+      end
+    end
+  end
+  
+  # Defines the has_one association
+  def defines_has_one_finder_method(method_name, association_model)
+    ivar_name = :"@#{method_name}"
+
+    define_method(method_name) do
+      if instance_variable_defined?(ivar_name)
+        instance_variable_get(ivar_name)
+      elsif attributes.include?(method_name)
+        attributes[method_name]
+      else
+        raise(ActiveResource::MissingPrefixParam, "#{self.class} has_one :#{method_name} requires that #{association_model} has a prefix set") if '/' == association_model.prefix
+        association_model.send(:include, ActiveResource::Singleton) unless association_model.respond_to?(:singleton_name)
+        instance_variable_set(ivar_name, association_model.find(:params => {:"#{self.class.element_name}_id" => self.id}))
       end
     end
   end
